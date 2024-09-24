5f22ac680b98b29db213d640ae4aadcc0033f029
khaidq <khaidq@ubuntu.myguest.virtualbox.org>
2024-09-24 13:20:31 +0700

    add pick_objects test

---
+++ scripts/pick_objects.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export TURTLEBOT3_MODEL='waffle_pi'
+
+xterm  -e  " roslaunch turtlebot3_gazebo turtlebot3_world.launch " &
+sleep 10
+
+xterm  -e  " roslaunch turtlebot3_navigation turtlebot3_navigation.launch " &
+sleep 10
+
+xterm  -e  " rosrun pick_objects pick_objects_node " &
+sleep 5
+
